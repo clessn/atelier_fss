3cd08d9b4463792e3506975981a10d3ea7c7ff33
Laurence-Olivier M. Foisy <mail@mfoisy.com>
2023-08-26 21:58:11 -0400

    rename()

--- Code/control_variables_assembly.r
+++ Code/control_variables_assembly.r
@@ -26,7 +26,15 @@ data_regression <- readRDS(paste0(data_path, "data_descriptive.RDS"))
 unique(data_regression$media_country)
 
 data_regression_controls <- merge(data_regression, data_controls, 
-                         by = "media_country", all = TRUE)
+                         by = "media_country", all = TRUE) %>%
+                         rename(christian100 = chrpc_RCS,
+                                muslim100 = muspc_RCS,
+                                non_religious100 = nrepc_RCS,
+                                information_flow = info_flow_KOF,
+                                globalization_index = overallGlob_index_KOF,
+                                migrant100 = migrant_per_WDI,
+                                pop_density = pop_den_WDI,
+                                pop_urban_density = pop_urb_WDI)
 
 saveRDS(data_regression_controls, paste0(data_path, 
                                 "control_dataset/data_regression_controls.RDS"))
