f7e3d0e80a8a2ab5ff140591b32ba01fa3e1b5ca
LaurenceOMFoisy <mail@mfoisy.com>
2024-04-10 16:18:58 -0400

    mod repo

--- Code/graphs/time_series.R
+++ Code/graphs/time_series.R
@@ -8,14 +8,6 @@ ggplot(data, aes(x = date, y = tone_index, color = country)) +
   scale_x_date(limits = c(as.Date("2013-01-02"), NA)) +
   theme_classic() +
   labs(x = "Date", y = "Tone", color = "Country") +
-  scale_color_manual(values = c("Syria" = "#CE1126", "Iraq" = "#007A3D", "Ukraine" = "#0057B7"))
+  scale_color_manual(values = c("Syrie" = "#CE1126", "Iraq" = "#007A3D", "Ukraine" = "#0057B7"))
 
-ggsave("_SharedFolder_article_syrie-ukraine/graphs/v4_21_02_2024/time_series.png", width = 10, height = 6, units = "in")
-
-data_syrie <- data %>% filter(country == "Syria")
-data_ukraine <- data %>% filter(country == "Ukraine") 
-data_iraq <- data %>% filter(country == "Iraq")
-
-syria_end_data <- max(as.Date(data_syrie$date))
-ukraine_end_data <- max(as.Date(data_ukraine$date))
-iraq_end_data <- max(as.Date(data_iraq$date))
+ggsave("data/graphs/time_series.png", width = 10, height = 6, units = "in")
